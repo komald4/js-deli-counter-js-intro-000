7fa09c51f603f2c59e0b08974ed7685204beae24
calm-fork-7535 <komald.4@gmail.com>
2020-01-13 04:29:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+# outputs a number. "You are ticket number X"
+# x++ for another customer
+
+#customer1, grab ticket
+#customer2, grab ticket++
+
+#Welcome, please grab a ticket for your order
+#20 customers
+#function ()
+#.push add onto the array
+#no one in the store but already had 100 people
+
+
+var katzDeli = [];
+var newCustomer = [];
+function takeANumber (currentLine, newCustomer){
+  var ticketInLine = currentLine.length +1; 
+  # var ticketInLine = newCustomer++
+  var newCustomer = newCustomer.length +1; 
+  var outputString = "Welcome, " + name. "You are ticket " + currentInLine + " in line.";
+  currentLine.push(currentLine);
+  newCustomer.push(newCustomer);
+  return outputString;
+}
